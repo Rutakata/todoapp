d8d0280bbe52aa63b4dbae132fff02f480b8dbb0
Rutakata <vladpelishenko14@gmail.com>
2022-12-14 18:59:55 +0200

    CRA and installation of needed packages

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,29 @@
 import React from 'react';
-import logo from './logo.svg';
+import styled from 'styled-components';
 import './App.css';
 
-function App() {
+
+const Container = styled.div`
+  padding: 0;
+  width: 70%;
+  margin: 0 auto;
+`;
+
+const Header = styled.h1`
+  margin: 50px 0 0 0;
+  height: 50px;
+  background-color: #ececec;
+  border: 1px solid #bababa;
+  padding: 0 0 0 20px;
+  font-size: 24px;
+  color: #4b4b4b;
+`;
+
+const App:React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>ToDos 5</Header>
+    </Container>
   );
 }
 
---
+++ src/styles/global.ts
@@ -0,0 +1,7 @@
+import { createGlobalStyle } from "styled-components"
+
+export default createGlobalStyle`
+    #root{
+        margin:0 auto;
+    }
+`
